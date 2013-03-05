01d59789b14750c77bafa92dd18e1594d60fed79
Mark Durrant <mark@pebblecode.com>
2013-03-05 16:08:13 +0000

    style chat

--- BackEnd/ThinkSprint/ThinkSprint/Models/QuestionProvider.cs
+++ BackEnd/ThinkSprint/ThinkSprint/Models/QuestionProvider.cs
@@ -10,10 +10,10 @@ namespace ThinkSprint.Models
         public Question CurrentQuestion { get; private set; }
         private List<Question> _questions = new List<Question>()
             {
-                new Question {Answer = 3, Options = new List<string>() {"1", "50", "2", "3"}, Text = "2 + 1"},
+                new Question {Answer = 3, Options = new List<string>() {"1", "10", "2", "3"}, Text = "2 + 1"},
                 new Question {Answer = 4, Options = new List<string>() {"1", "8", "4", "3"}, Text = "6 - 2"},
-                new Question {Answer = 4, Options = new List<string>() {"4", "50", "2", "3"}, Text = "2 × 2"},
-                new Question {Answer = 2, Options = new List<string>() {"1", "50", "2", "3"}, Text = "6 ÷ 3"},
+                new Question {Answer = 4, Options = new List<string>() {"4", "7", "2", "3"}, Text = "2 × 2"},
+                new Question {Answer = 2, Options = new List<string>() {"1", "6", "2", "3"}, Text = "6 ÷ 3"},
             };
 
         private int i = 0;
