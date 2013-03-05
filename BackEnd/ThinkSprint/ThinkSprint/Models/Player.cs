c3b2a7e2f77939c4d257c900cc4be7129a68fdd6
Tom <toms.squires@pebblecode.com>
2013-03-05 13:53:24 +0000

    a message

---
+++ BackEnd/ThinkSprint/ThinkSprint/Models/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ThinkSprint.Models
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+    }
+}
---
+++ BackEnd/ThinkSprint/ThinkSprint/Models/Result.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ThinkSprint.Models
+{
+    public class Result
+    {
+        public bool Correct { get; set; }
+        public List<Player> Players { get; set; }
+    }
+    
+}
